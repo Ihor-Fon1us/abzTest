62053cef68bb14019964982f5264caff345940e7
Ihor <igor.rakitin.2000@gmail.com>
2022-10-17 16:06:39 +0300

    fixed error handler

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const OpenApiValidator = require('express-openapi-validator');
 const config = require('./bin/config');
 
 const indexRouter = require('./routes/index');
+const { HttpError } = require('express-openapi-validator/dist/framework/types');
 
 const folder = config.PHOTO_FOLDER;
 fs.access(folder, (err) => {
@@ -19,12 +20,6 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(
-  OpenApiValidator.middleware({
-    apiSpec: './api/openapi.yaml',
-    ignoreUndocumented: true
-  }),
-);
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.text());
 app.use(bodyParser.json());
@@ -32,11 +27,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('./images'));
-
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: './api/openapi.yaml',
+    ignoreUndocumented: true
+  }),
+);
 app.use('/', indexRouter);
 
 // error handler
 app.use(function (error, req, res, next) {
+  if (error instanceof HttpError) {
+    error.status = 422;
+    error.message = "Validation failed";
+    error.fails = error.errors.reduce((a, v) => ({ ...a, [v.path.split(".")[v.path.split(".").length - 1]]: v.message}), {}); 
+    error.errors = undefined;
+  }
   res.status(error.status || 500).json({
     success: false,
     message: error.message,
