07eb5ba54346ab91a40bdea037cef1a35879e859
Ihor-Fon1us <60787479+Ihor-Fon1us@users.noreply.github.com>
2022-10-17 18:41:01 +0300

    Update config.js

--- bin/config.js
+++ bin/config.js
@@ -3,7 +3,7 @@ module.exports = {
     POSTGRES_USER: process.env.POSTGRES_USER || "root",
     POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD || "root",
     POSTGRES_DB: process.env.POSTGRES_DB || "test_db",
-    POSTGRES_HOST: process.env.POSTGRES_HOST || "172.17.0.1",
+    POSTGRES_HOST: process.env.POSTGRES_HOST || "127.0.0.1",
     COST_JWT: process.env.COST_JWT || "testcost",
     PHOTO_FOLDER: process.env.PHOTO_FOLDER || "./images/users",
     TINIFY_KEY: process.env.TINIFY_KEY || "qSy8Bxxm6Zym101zzl643PBFtqVQbTKB"
